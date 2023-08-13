208713ed8b3c6dec36c22dd79d00def2b9b2e94d
AngelEsquit <esq23221@uvg.edu.gt>
2023-08-13 12:33:23 -0600

    Create Comprador.java

---
+++ Comprador.java
@@ -0,0 +1,38 @@
+public class Comprador {
+    private String nombre;
+    private String email;
+    private int numBoletos;
+    private int presupuesto;
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setNumBoletos(int numBoletos) {
+        this.numBoletos = numBoletos;
+    }
+
+    public int getNumBoletos() {
+        return numBoletos;
+    }
+
+    public void setPresupuesto(int presupuesto) {
+        this.presupuesto = presupuesto;
+    }
+
+    public int getPresupuesto() {
+        return presupuesto;
+    }
+}
